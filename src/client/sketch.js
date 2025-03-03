7caa70c3e151d04af17b776875b6179d9d2cb1d1
bb07461 <98509031+bb07461@users.noreply.github.com>
2025-03-03 12:59:43 -0500

    added cheat button and some logic
    not sure if its not loading cause i wasnt running with node

--- src/client/sketch.js
+++ src/client/sketch.js
@@ -23,7 +23,9 @@ function draw() {
   drawContestant()
   drawStars(30,30,5, score)
   drawLights()
+  cheater()
   if(applauseVis) drawApplause()
+    
 
   
 }
@@ -36,6 +38,42 @@ const cheat = () => {
   fill("red"); // Set fill color to red
  // ellipse(hostPos, height, 50, 50); 
 }
+
+
+function cheater(){
+  
+  const chanceCheat = 0.5;
+  const cheating = random()
+
+  fill("red");
+  drawCheat()
+
+  // if chance of it happening within bounds to trigger and 
+  // state that it just move 
+  //if not true sets visible to true 
+  if(cheating < chanceCheat && !cheatVis ){
+    moveCheat()
+  }
+  
+}
+
+let cheatX = 0
+let cheatVis = false // is cheat visible
+
+function drawCheat() {
+  fill(255,0,0)
+  ellipse(cheatX, 0, 100 , 100) 
+}
+
+function moveCheat() {
+    cheatX = random(80, 250);
+    cheatVis = true;
+}
+
+const hideCheat = () => {
+   cheatVis = false;
+}
+
 // Audience applause when board lights up same as speech bubble 
 // Over random audience member for the list 
 function audience(){
@@ -93,6 +131,8 @@ function hostInteract(){
   if (hostPos >= width || hostPos <= 0) {
     speed *= -1;  // Flip the direction
   }
+
+  
 }
 // contestant podium lights up 
 function drawContestant(){
@@ -120,7 +160,8 @@ function keyPressed() {
   } else if (key === 'a') {
     hideApplause()
   } else if (key === 'c') {
-    cheat()
+    cheater()
+    
   }
 }
 
