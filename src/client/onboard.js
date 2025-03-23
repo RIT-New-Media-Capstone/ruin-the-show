8209caee8ce3b714e2e18aed340f4b05a15c1c89
Ugochinyere Okogeri <98509031+bb07461@users.noreply.github.com>
2025-03-23 15:56:42 -0400

    setting up logic for onboard
    in progress

---
+++ src/client/onboard.js
@@ -0,0 +1,23 @@
+
+//host introduction sequence
+// display applause screen, if app
+
+//screen variables 
+let applauseBtnScreen; // image variables
+let hostLvrScreen;
+let contestantBtnScreen;
+
+//user inputs 
+let hostJoystick;
+curtainLever;
+smBtn1; 
+smBtn2;
+smBtn3;
+smBtn4;
+
+
+//
+if (applauseBtnScreen){
+    //switch screen
+
+}
